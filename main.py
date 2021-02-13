a775c9955f70394ef52e641900d95f86f3e06eb4
cap1n3m0 <vedantj1203@gmail.com>
2021-02-12 21:53:22 -0500

    update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from tkinter import *
 import pyshorteners 
 import webbrowser
+import socket
 
 gui = Tk() 
 
 enteredText = ""
+hostName = socket.gethostname()
+IP = socket.gethostbyname(hostName)
 
+URLS = [] 
+    
 class GUI: 
     title = Label(gui, text="URl Shortener")
     URlBox = Entry(gui, bd = 5)
@@ -25,13 +30,22 @@ def shortenURL(URL):
     global short 
     global g 
     global enteredText
+    isThere = False
     enteredText = g.URlBox.get()
     s = pyshorteners.Shortener()
     short = s.tinyurl.short(URL)
-    print("Short is: " + short) 
-    g.giveNewURL.labelText = "Success! Your URL has been shortened! You may now use this one: " + short
-    g.giveNewURL.pack()
-    openURL(URL)
+    for s in URLS:
+        if s[0] == IP and s[1] == short:
+            print("Already exists: " + s[1])
+            isThere = True
+
+    if not isThere: 
+        newValue = (IP, short) 
+        URLS.add(newValue)
+        print("Short is: " + short) 
+        g.giveNewURL.labelText = "Success! Your URL has been shortened! You may now use this one: " + short
+        g.giveNewURL.pack()
+        openURL(URL)
 
 g.title.pack()
 g.URlBox.pack()
